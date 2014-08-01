52d1d65a2252a869ddafe6f7f37090845452b9fe
Antony Riley <antony@cyberiantiger.org>
2014-08-01 11:37:07 +0300

    Clean up imports.

--- src/main/java/org/cyberiantiger/minecraft/duckchat/irc/Main.java
+++ src/main/java/org/cyberiantiger/minecraft/duckchat/irc/Main.java
@@ -5,7 +5,6 @@
 package org.cyberiantiger.minecraft.duckchat.irc;
 
 import org.cyberiantiger.minecraft.duckchat.command.SubCommand;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
