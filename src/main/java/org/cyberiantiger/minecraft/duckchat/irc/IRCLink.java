8fe6ac0f0a82a267000bf25f76a8bd220a49c431
Antony Riley <antony@cyberiantiger.org>
2014-08-01 11:19:37 +0300

    Disable SO_TIMEOUT.
    Fucking third party code.

--- src/main/java/org/cyberiantiger/minecraft/duckchat/irc/IRCLink.java
+++ src/main/java/org/cyberiantiger/minecraft/duckchat/irc/IRCLink.java
@@ -300,7 +300,8 @@ public class IRCLink {
         ircConnection.setDaemon(true);
         ircConnection.setColors(true);
         ircConnection.setPong(true);
-        ircConnection.setDebug(true);
+        //ircConnection.setDebug(true);
+        ircConnection.setTimeout(0);
         ircConnection.addIRCEventListener(ircEventAdapter);
         ircConnection.setUncaughtExceptionHandler(
                 new Thread.UncaughtExceptionHandler() {
